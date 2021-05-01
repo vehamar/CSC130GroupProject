a1027fa6095f87c33c1284077c2dda99cab6e58e
mlm0207 <54418596+mlm0207@users.noreply.github.com>
2021-05-01 19:14:52 -0400

    Add files via upload

---
+++ HotDogStandApp.py
@@ -0,0 +1,9 @@
+from HotDogStand import HotDogStand
+
+def main():
+    print("Welcome to Talon's HotDog Stand!")
+    hds = HotDogStand()
+    hds.displayActions()
+
+if __name__ == "__main__":
+    main()
